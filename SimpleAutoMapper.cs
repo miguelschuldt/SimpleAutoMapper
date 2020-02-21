00b5f0a1339bb11647a3af62c23ac197b4ef7d68
unknown <900193@proway.treina>
2020-02-21 13:51:05 -0300

    first commit

---
+++ SimpleAutoMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace SimpleAutoMapper
+{
+    public class SimpleAutoMapper<W> where W : class, new()
+    {
+        public W Map(object origin)
+        {
+            Type WType = typeof(W);
+            Type OriginType = origin.GetType();
+            W w = new W();
+            foreach (PropertyInfo property in WType.GetProperties())
+            {
+                PropertyInfo OriginProperty = OriginType.GetProperty(property.Name);
+                if (OriginProperty != null) 
+                {
+                    try
+                    {
+                        property.SetValue(w, OriginProperty.GetValue(origin));
+                    }
+                    catch { }
+                }
+            }
+            return w;
+        }
+    }
+}
